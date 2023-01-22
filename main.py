ac22c500baf87c9d45268be362b2d9f135c58ad8
kwanmui107 <106944192+kwanmui107@users.noreply.github.com>
2023-01-21 23:25:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,37 +10,42 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+#create blanks for holding the password in each category
 rd_letters = ""
 rd_letter = ""
 rd_symbol = ""
 rd_symbols = ""
 rd_number = ""
 rd_numbers = ""
+#Create the random letters based on the quantity input
 for nr_letter in range (0,nr_letters):
   rd_chooser = random.randint(0,51)
   rd_letter = letters[rd_chooser]
   rd_letters += rd_letter
 print(rd_letters)
 
-
+#Create the random symbols based on the quantity input
 for nr_symbol in range (0,nr_symbols):
   rd_chooser = random.randint(0,8)
   rd_symbol = symbols[rd_chooser]
   rd_symbols += rd_symbol
 print(rd_symbols)
-  
+
+#Create the random numbers based on the quantity input
 for nr_number in range (0,nr_numbers):
   rd_chooser = random.randint(0,9)
   rd_number = numbers[rd_chooser]
   rd_numbers += rd_number
 print(rd_numbers)  
 
+#combine the string of letters, numbers and symbols
 password = list(rd_letters+rd_symbols+rd_numbers)
-print(type(password))
+#shuffle the password
 random.shuffle(password)
+#creates a variable password_str to hold the string
 password_str=""
+#converts the list back to a string so it can be held in the variable
 for passwords in password:
   password_str += passwords
+#prints the password in string format
 print(password_str)
